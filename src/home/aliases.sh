f1b9e0bb5d47865c11469f355874e8a3f32d8da8
Christian Grete <webmaster@christiangrete.com>
2019-04-12 16:45:25 +0200

    docs(*): add comments

--- src/home/aliases.sh
+++ src/home/aliases.sh
@@ -1,13 +1,34 @@
+#
+# The ~/.aliases dotfile:
+# Provides a basic set of POSIX compliant aliases.
+#
+
+#
+# Faster navigation with cd
+#
+
 alias ..='cd ..'
 alias ...='cd ../..'
 alias ....='cd ../../..'
 alias .....='cd ../../../..'
 
+#
+# Faster clearing, when available
+#
+
 if command -v 'clear' > '/dev/null' 2>&1; then
   alias c='clear'
 fi
 
+#
+# Shortcut to print a long list of directory contents
+#
+
 alias ll='ls -la'
+
+#
+# Faster logout, when available
+#
 
 if command -v 'logout' > '/dev/null' 2>&1; then
   alias lo='logout'
