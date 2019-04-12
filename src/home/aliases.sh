b82c38e8e816e75d427d82bccb47c57d4f41b162
Christian Grete <webmaster@christiangrete.com>
2019-04-12 16:08:17 +0200

    feat(*): extend base aliases, make conditional

--- src/home/aliases.sh
+++ src/home/aliases.sh
@@ -1,5 +1,14 @@
 alias ..='cd ..'
 alias ...='cd ../..'
 alias ....='cd ../../..'
-alias c='command clear'
-alias lo='command logout'
+alias .....='cd ../../../..'
+
+if command -v 'clear' > '/dev/null' 2>&1; then
+  alias c='clear'
+fi
+
+alias ll='ls -la'
+
+if command -v 'logout' > '/dev/null' 2>&1; then
+  alias lo='logout'
+fi
