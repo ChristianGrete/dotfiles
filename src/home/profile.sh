b2c4f4148fd6175aed863c608343c5823e002f28
Christian Grete <webmaster@christiangrete.com>
2019-04-15 15:23:52 +0200

    docs(*): improve note

--- src/home/profile.sh
+++ src/home/profile.sh
@@ -7,8 +7,9 @@
 # Defining the ENV file
 #
 
-# Set the ~/.rc dotfile to be the ENV file and name it in SHINIT as well (for portability)
+# Set the ~/.rc dotfile to be the ENV file and name it in SHINIT as well (for portability) [1]
 export ENV="$HOME/.rc"
 export SHINIT="$ENV"
 
-# Note: These lines should be left on the very end of this file to make clear that the ~/.rc file is parsed afterwards
+# NOTES:
+#  [1]  These lines should be left on the very end of this file to make clear that the ~/.rc file is parsed afterwards.
