76c9c37cd985d00e13d67dae3d024b326966509b
Christian Grete <webmaster@christiangrete.com>
2019-04-15 17:17:57 +0200

    docs(*): improve notes

--- src/home/rc.sh
+++ src/home/rc.sh
@@ -14,13 +14,13 @@
 # 2. Optimizing the PATH variable
 #
 
-# Temporary reset the PATH variable to its default value
+# Temporary reset the PATH variable to its default value [3]
 PATH="$(command -p getconf PATH)"
 
 # Start designing a new PATH variable from scratch
 __dotfiles_rc__optimized_PATH=''
 
-# Exclude symlinked /bin from systemd file hierarchy
+# Exclude symlinked /bin from systemd file hierarchy [4]
 [ ! -L '/bin' -a -e '/bin' ] && __dotfiles_rc__optimized_PATH=':/bin'
 
 # The secondary hierarchy should always be present
@@ -43,7 +43,7 @@ unset __dotfiles_rc__optimized_PATH
 # Include the ~/.bin directory provided by the dotfiles base package
 [ -d "$HOME/.bin" -a -r "$HOME/.bin" ] && PATH="$HOME/.bin:$PATH"
 
-# Include the fixed ~/.local directory when present (quaternary hierarchy)
+# Include the fixed ~/.local directory when present (quaternary hierarchy) [5]
 [ -d "$HOME/.local/bin" -a -r "$HOME/.local/bin" ] && PATH="$HOME/.local/bin:$PATH"
 
 #
@@ -56,7 +56,7 @@ export PATH
 # 5. Importing the dotfiles base package runcom
 #
 
-# Load the generated ~/.rc.d/localhost/dotfiles.sh file provided by the doftiles base package [3]
+# Load the generated ~/.rc.d/localhost/dotfiles.sh file provided by the doftiles base package [6]
 [ -r "$HOME/.rc.d/localhost/dotfiles.sh" ] && . "$HOME/.rc.d/localhost/dotfiles.sh"
 
 #
@@ -79,6 +79,12 @@ export PATH
 #       fails when the user decides to set a blank prompt.
 #  [2]  Since `test -t 0` fails when invoked remotely via SSH (see https://www.tldp.org/LDP/abs/html/intandnonint.html),
 #       it is also checked whether stdin is instead a named pipe.
-#  [3]  The ~/.rc.d/localhost/dotfiles.sh runcom provides major dotfiles environment variables that are used by dotfiles
+#  [3]  The getconf utility may needs to have a proper PATH set up before it can be executed (see
+#       http://pubs.opengroup.org/onlinepubs/9699919799/utilities/command.html).
+#  [4]  /bin, /sbin and /usr/sbin are symlinks pointing to /usr/bin in the systemd file hierarchy (see
+#       https://www.freedesktop.org/software/systemd/man/file-hierarchy.html#Compatibility%20Symlinks).
+#  [5]  The systemd file hierarchy also defines a fixed directory structure for high-level user resources (see
+#       https://www.freedesktop.org/software/systemd/man/file-hierarchy.html#Home%20Directory).
+#  [6]  The ~/.rc.d/localhost/dotfiles.sh runcom provides major dotfiles environment variables that are used by dotfiles
 #       itself and dotfiles packages that have been installed in addition.
 #
