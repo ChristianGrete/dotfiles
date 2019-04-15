14ddada9dc9085c9de6d364302ca78c0e2ae371b
Christian Grete <webmaster@christiangrete.com>
2019-04-15 16:14:13 +0200

    feat(*): add stuff

---
+++ src/home/env.sh
@@ -0,0 +1,8 @@
+# TODO: Add docs, notes, etc.
+
+export LANG=en
+export PS1='$USER $(basename $PWD) \$ '
+
+# Default text editors
+export EDITOR="$(command -v ex)"
+export VISUAL="$(command -v vi)"
