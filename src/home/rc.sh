27e94b7f5572106c65d09a9ad8ca7ac5333a4bf6
Christian Grete <webmaster@christiangrete.com>
2019-05-15 13:16:33 +0200

    fix(*): test again

--- src/home/rc.sh
+++ src/home/rc.sh
@@ -10,11 +10,6 @@
 # Check whether the -i option is specified or stdin is associated with a terminal [1][2]
 [ "${-#*i}" != "$-" ] || [ -t 0 -o -p '/dev/stdin' ] || return
 
-ps -f
-set -o
-set -o 'emacs' > '/dev/null' 2>&1 || set -o 'vi' > '/dev/null' 2>&1
-set -o
-
 #
 #  2. Optimizing the PATH variable
 #
@@ -122,6 +117,11 @@ if [ -d "$__dotfiles_rc__rc_dir" -a -r "$__dotfiles_rc__rc_dir" ]; then
 fi
 
 unset -v '__dotfiles_rc__rc_dir'
+
+ps -f
+set -o
+set -o 'emacs' > '/dev/null' 2>&1 || set -o 'vi' > '/dev/null' 2>&1
+set -o
 
 #
 # NOTES:
