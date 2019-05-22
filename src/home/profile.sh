2f2317393d5b02904e8b6366bfa36ce4fbd5489e
Christian Grete <webmaster@christiangrete.com>
2019-05-22 10:08:18 +0200

    style(*): limit to 80 cols

--- src/home/profile.sh
+++ src/home/profile.sh
@@ -4,14 +4,16 @@
 #
 
 #
-# Defining the ENV file
+# Define the `$ENV` file
 #
 
-# Set the ~/.rc dotfile to be the ENV file and name it in SHINIT as well (for portability) [1]
+# Set the "~/.rc" dotfile to be the `$ENV` file and reference it in `$SHINIT` as
+# well (for portability reasons)
 export ENV="$HOME/.rc"
 export SHINIT="$ENV"
 
 #
-# NOTES:
-#  [1]  These lines should be left on the very end of this file to make clear that the ~/.rc file is parsed afterwards.
+# NOTE:
+#   The two lines above should be left on the very end of this file to make
+#   clear that the "~/.rc" file is parsed afterwards.
 #
